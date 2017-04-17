eec5894b1346188b7225110f380935638f42ea46
Allen Zhong <zhong.allen@gmail.com>
2017-04-17 23:01:32 +1200

    add username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  # before_action :authenticate_user!
-  # layout :dynamic_layout
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+  layout :dynamic_layout
+
+  protected
 
   def dynamic_layout
   	user_signed_in? ? 'application' : 'authentication'
+  end
+
+  def configure_permitted_parameters
+  	permit_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
+  	devise_parameter_sanitizer.permit :sign_up, keys: permit_attrs
+  	devise_parameter_sanitizer.permit :account_update, keys: permit_attrs
   end
 end
