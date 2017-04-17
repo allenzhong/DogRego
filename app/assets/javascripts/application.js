00f45092b903de63eff9c788e445005a9096e615
Allen Zhong <zhong.allen@gmail.com>
2017-04-17 15:23:38 +1200

    fix merge problems

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,4 @@
 //= require jquery_ujs
 //= require bootstrap_sb_admin_base_v2
 //= require turbolinks
-//= require bootstrap_sb_admin_base_v2
 //= require_tree .
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
-  layout :dynamic_layout
+  # before_action :authenticate_user!
+  # layout :dynamic_layout
 
   def dynamic_layout
   	user_signed_in? ? 'application' : 'authentication'
