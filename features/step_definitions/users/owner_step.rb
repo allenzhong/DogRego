41f8f376a9dd2912f518dca75e107bba66566112
Allen Zhong <zhong.allen@gmail.com>
2017-04-19 20:32:00 +1200

    add acceptance test

---
+++ features/step_definitions/users/owner_step.rb
@@ -0,0 +1,49 @@
+Given(/^I'm an registered owner$/) do
+  @user = User.create!({
+    username: 'admin',
+    email: 'admin@admin.com',
+    password: '12345678',
+    password_confirmation: '12345678'
+  })
+  @user.save!
+end
+
+Given(/^I have login on$/) do
+  visit new_user_session_path
+  fill_in 'user_login', with: @user.username
+  fill_in 'user_password', with: '12345678'
+  click_button 'Log in'
+  expect(page).to have_content("Signed")
+end
+
+Given(/^I'm on profile page$/) do
+  visit edit_user_path(@user)
+end
+
+When(/^I fill up my first name, middle name and last name with "([^"]*)", "([^"]*)" and "([^"]*)"$/) do |first, middle, last|
+  fill_in 'user_first_name', with: first 
+  fill_in 'user_middle_name', with: middle 
+  fill_in 'user_last_name', with: last 
+end
+
+When(/^Click 'Save'$/) do
+  click_button "Save"
+end
+
+Then(/^The page should give me notification "([^"]*)"$/) do |save|
+  expect(page).to have_content(save)
+end
+
+When(/^Click "([^"]*)" to deregister$/) do |arg1|
+  click_button deregister
+end
+
+When(/^Click "([^"]*)"$/) do |arg1|
+  wait = Selenium::WebDriver::Wait.new ignore: Selenium::WebDriver::Error::NoAlertPresentError
+  alert = wait.until { page.driver.browser.switch_to.alert  }
+  alert.accept
+end
+
+Then(/^The page should redirect to login page$/) do
+  expect(page).to have_content("Sign in") 
+end
---
+++ features/users/owner.feature
@@ -0,0 +1,21 @@
+Feature: Owner
+As a owner,
+I want to visit my profile
+so that I can enter, save, update or delete my details
+
+Background:
+	Given I'm an registered owner
+	Given I have login on
+	Given I'm on profile page
+
+Scenario: Enter/Update owner's detail
+	When I fill up my first name, middle name and last name with "Sathish", "Prabhu" and "Vijaya Kumar"
+	And Click 'Save'
+	Then The page should give me notification "Saved"
+
+
+@javascript
+Scenario: deregister owner
+	When Click "Deregister" to deregister
+	And Click "OK"
+	Then The page should redirect to login page 
