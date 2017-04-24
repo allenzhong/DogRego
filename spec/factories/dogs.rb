992ac8f1967b361ee6634cf408aa5011d440c768
Allen Zhong <zhong.allen@gmail.com>
2017-04-24 15:06:38 +1200

    add dog model and rspec

---
+++ spec/factories/dogs.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :dog do
+  	#Oh no, I'm not a cat T_T
+    name { Faker::Cat.name } 
+    breed { Faker::Cat.breed } 
+    date_of_birth { Faker::Date.birthday(0,15) }
+    association :user 
+  end
+end
