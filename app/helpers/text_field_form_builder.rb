a58f2226cd5c8b3286e572e3fdd536757d3f6729
Allen Zhong <zhong.allen@gmail.com>
2017-04-27 00:04:47 +1200

    invoke self method instead of _tag method

--- app/helpers/text_field_form_builder.rb
+++ app/helpers/text_field_form_builder.rb
@@ -4,19 +4,11 @@ class TextFieldFormBuilder < ActionView::Helpers::FormBuilder
   include ActionView::Helpers::TextHelper
 
 
-  def div_radio_button(method, tag_value, options = {})
-    @template.content_tag(:div,
-      @template.radio_button(
-        @object_name, method, tag_value, objectify_options(options)
-      )
-    )
-  end
-
   def text_field_with_label(label, attribute, placeholder=nil, options={})
   	@template.content_tag :div, class: 'form-group-with-height' do
   		label = @template.content_tag(:label, label, class: 'col-sm-2 control-label')
   		input = @template.content_tag(:div, class: 'col-sm-10') do
-  			@template.text_field_tag(attribute, @object.send(attribute), placeholder: placeholder, class: 'form-control')
+  			self.text_field(attribute, { placeholder: placeholder, class: 'form-control' })
 			end
 
       label + input
@@ -30,7 +22,7 @@ class TextFieldFormBuilder < ActionView::Helpers::FormBuilder
 
       attributes.map.with_index do |ele,i|
         @template.concat(@template.content_tag(:div,
-          (@template.text_field_tag(ele, @object.send(ele), placeholder: ele.to_s.humanize.titleize, class: 'form-control')),
+          (self.text_field(ele, { placeholder: ele.to_s.humanize.titleize, class: 'form-control'})),
           class: "col-sm-#{percentage[i]}"))
       end
     end
@@ -41,7 +33,7 @@ class TextFieldFormBuilder < ActionView::Helpers::FormBuilder
     @template.content_tag :div, class: 'form-group-with-height' do
       @template.concat @template.content_tag(:label, label, class: 'col-sm-2 control-label')
       @template.concat(@template.content_tag(:div, class: 'col-sm-10') do
-        @template.concat @template.password_field_tag(attribute, @object.send(attribute), placeholder: placeholder, class: 'form-control')
+        @template.concat self.password_field(attribute, { placeholder: placeholder, class: 'form-control'})
         @template.concat @template.content_tag(:i, extra)
         @template.concat @template.capture(&block) if block_given?
       end)
