861646f3f085d4f9e5b30994660433933b2be385
Allen Zhong <zhong.allen@gmail.com>
2017-04-27 21:23:41 +1200

    add datepicker

--- app/helpers/text_field_form_builder.rb
+++ app/helpers/text_field_form_builder.rb
@@ -39,6 +39,21 @@ class TextFieldFormBuilder < ActionView::Helpers::FormBuilder
     end
   end
 
+  def date_picker(label, attribute, options={})
+    @template.content_tag :div, class: 'form-group-with-height' do
+      @template.concat @template.content_tag(:label, label, class: 'col-sm-2 control-label')
+      @template.concat(@template.content_tag(:div, class: 'col-sm-10') do
+        @template.concat(@template.content_tag(:div, {id: 'datepicker', class: 'input-group date'}) do
+          @template.concat self.text_field(attribute, { class: 'form-control' })
+          @template.concat(@template.content_tag(:span, class: 'input-group-addon') do
+            @template.concat @template.content_tag(:span, nil, class: 'glyphicon glyphicon-calendar')
+          end)
+        end)
+      end)
+      @template.concat(@template.javascript_tag(datepicker_javascript))
+    end
+  end
+
   def submit_with_style(attribute, options={})
     @template.content_tag :div, class: 'form-group-with-height actions' do
       @template.content_tag :div, class: 'col-sm-offset-2 col-sm-2 col-xs-12' do
@@ -52,4 +67,15 @@ class TextFieldFormBuilder < ActionView::Helpers::FormBuilder
   # 		@template.text_field method, tag_value, {css: 'form-control'}
   # 	end	
   # end
+  private
+
+  def datepicker_javascript
+    <<-JS
+      $(function () {
+        $('#datepicker').datetimepicker({
+          format: 'DD/MM/YYYY'
+        });
+      });
+    JS
+  end
 end
