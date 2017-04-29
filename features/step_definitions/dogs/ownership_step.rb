3f1576f8b077ac1c48d4827cdd7549c1949a2295
Allen Zhong <zhong.allen@gmail.com>
2017-04-29 16:52:12 +1200

    add acceptance test

---
+++ features/step_definitions/dogs/ownership_step.rb
@@ -0,0 +1,36 @@
+Given(/^I have logged in$/) do
+	@user = User.create!({
+    username: 'admin1',
+    email: 'admin1@admin.com',
+    password: '12345678',
+    password_confirmation: '12345678'
+  })
+  @user.save!
+  visit new_user_session_path
+  fill_in 'user_login', with: @user.username
+  fill_in 'user_password', with: '12345678'
+  click_button 'Log in'
+  expect(page).to have_content("Signed")
+end
+
+Given(/^I have one dog created$/) do
+	@dog = FactoryGirl.create(:dog)	
+	visit dogs_path
+end
+
+When(/^I click 'Register' for my first dog$/) do
+	within '.table' do
+		click_on 'Register', match: :first
+	end
+end
+
+When(/^Select "([^"]*)" for registration$/) do |arg1|
+	expect(page).to have_content("12 months")
+	choose('group_12_month')
+	click_on 'Register My Dog'
+end
+
+Then(/^The page should show confirmation$/) do
+	expect(page).to have_content("is now registered for 12 months").
+	expect(page).to have_content("pay $85 to bank account 12-1234-1234-01").
+end
