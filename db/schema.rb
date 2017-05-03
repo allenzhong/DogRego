368b67a1efa481c7e52e4212c08abfd28c7df23c
Allen Zhong <zhong.allen@gmail.com>
2017-05-03 22:40:44 +1200

    add start_on to ownership registration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170429050330) do
+ActiveRecord::Schema.define(version: 20170503103913) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20170429050330) do
     t.integer  "dog_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date     "start_on"
     t.index ["dog_id"], name: "index_ownership_registrations_on_dog_id", using: :btree
   end
 
