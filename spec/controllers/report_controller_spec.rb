e0343a3f7321cc3ad7ab916bd68de3c3955eb384
Allen Zhong <zhong.allen@gmail.com>
2017-06-02 19:56:01 +1200

    convert indent to spaces

--- spec/controllers/report_controller_spec.rb
+++ spec/controllers/report_controller_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe ReportController, type: :controller do
-	login_admin
+  login_admin
 
-	let(:user) { controller.current_user }
-	let(:dog) { FactoryGirl.create(:dog)}
-	let(:ownership_registration) {FactoryGirl.create(:ownership_registration)}
+  let(:user) { controller.current_user }
+  let(:dog) { FactoryGirl.create(:dog)}
+  let(:ownership_registration) {FactoryGirl.create(:ownership_registration)}
 
-	describe "GET #index" do
+  describe "GET #index" do
     it "assigns all dogs as @dogs" do
       get :index
       expect(assigns(:users)).to eq([dog.user, ownership_registration.dog.user])
