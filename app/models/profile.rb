a43eb321e7a4356ab0a724ab367b9860210b6cd1
Allen Zhong <zhong.allen@gmail.com>
2017-06-02 19:55:21 +1200

    convert indent to spaces

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,10 +2,10 @@ class Profile < ApplicationRecord
   belongs_to :user, inverse_of: :profile
 
   def name
-  	"#{first_name} #{last_name}"
+    "#{first_name} #{last_name}"
   end
 
   def address
-  	"#{street}, #{suburb}, #{town_city} #{postcode}"
+    "#{street}, #{suburb}, #{town_city} #{postcode}"
   end
 end
